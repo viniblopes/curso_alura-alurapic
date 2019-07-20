1b31747eb2931104951281f6c4ed761c2e259c1a
vinciusbecker <vbllopes@gmail.com>
2019-07-20 18:01:09 -0300

    Finalizando aula 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
+import { PhotosModule } from './photos/photos.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, PhotosModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
