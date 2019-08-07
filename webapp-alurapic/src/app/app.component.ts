c64e64a794dfbce60ed642eb7d388451dacfe215
vinciusbecker <vbllopes@gmail.com>
2019-08-07 07:45:19 -0300

    Fim aula 4 Alura

--- webapp-alurapic/src/app/app.component.ts
+++ webapp-alurapic/src/app/app.component.ts
@@ -5,21 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-
-  photos = [
-    { 
-      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg', 
-      description: 'Leão'
-    },
-    { 
-      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg', 
-      description: 'Leoa'
-    },
-    { 
-      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg', 
-      description: 'Leoa'
-    }         
-  ];  
-
-}
+export class AppComponent {}
--- webapp-alurapic/src/app/app.module.ts
+++ webapp-alurapic/src/app/app.module.ts
@@ -1,18 +1,16 @@
+import { ErrorsModule } from './errors/errors.module';
+import { AppRoutingModule } from './app.routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
+import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    PhotosModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, PhotosModule, AppRoutingModule, ErrorsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
