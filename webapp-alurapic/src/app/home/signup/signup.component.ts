57efdec4d6afea2106d89189b72c3ef7a7a813b9
Vinicius Becker <vbllopes@gmail.com>
2019-09-14 10:29:35 -0300

    Curso Angular parte 4 - Fim aula 06

--- webapp-alurapic/src/app/home/signup/signup.component.ts
+++ webapp-alurapic/src/app/home/signup/signup.component.ts
@@ -7,6 +7,7 @@ import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validat
 import { SignUpService } from './signup.service';
 import { Router } from '@angular/router';
 import { Title } from '@angular/platform-browser';
+import { userNamePasswordValidator } from './username-password.validator';
 
 @Component({
   templateUrl: './signup.component.html',
@@ -24,35 +25,50 @@ export class SignUpComponent implements OnInit {
     private platformDetectorService: PlatformDetectorService
   ) {}
   ngOnInit(): void {
-    this.signupForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      fullName: [
-        '',
-        [Validators.required, Validators.minLength(2), Validators.maxLength(40)]
-      ],
-      userName: [
-        '',
-        [
-          Validators.required,
-          lowerCaseValidator,
-          Validators.minLength(2),
-          Validators.maxLength(30)
+    this.signupForm = this.formBuilder.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        fullName: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(2),
+            Validators.maxLength(40)
+          ]
         ],
-        this.userNotTakenValidatorService.checkUserNameTaken()
-      ],
-      password: [
-        '',
-        [Validators.required, Validators.minLength(8), Validators.maxLength(14)]
-      ]
-    });
+        userName: [
+          '',
+          [
+            Validators.required,
+            lowerCaseValidator,
+            Validators.minLength(2),
+            Validators.maxLength(30)
+          ],
+          this.userNotTakenValidatorService.checkUserNameTaken()
+        ],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(8),
+            Validators.maxLength(14)
+          ]
+        ]
+      },
+      {
+        validator: userNamePasswordValidator
+      }
+    );
     this.platformDetectorService.isPlatformBrowser() &&
       this.inputEmail.nativeElement.focus();
   }
 
   signUp() {
-    const newUser = this.signupForm.getRawValue() as NewUser;
-    this.signUpService
-      .signUp(newUser)
-      .subscribe(() => this.router.navigate(['']), err => console.log(err));
+    if (this.signupForm.valid && !this.signupForm.pending) {
+      const newUser = this.signupForm.getRawValue() as NewUser;
+      this.signUpService
+        .signUp(newUser)
+        .subscribe(() => this.router.navigate(['']), err => console.log(err));
+    }
   }
 }
